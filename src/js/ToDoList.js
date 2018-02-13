b05a9220fc52fab8b4f70bdcd2fb3f5eaf6b0419
kikiondo <kikisvv@ucol.mx>
2018-02-13 15:32:31 -0600

    Se creó el metodo addTask

--- src/js/ToDoList.js
+++ src/js/ToDoList.js
@@ -1,11 +1,15 @@
 //LocalStorage tiene dos metodos, uno para establecer valores y otro para obtener valores  localStorage.getItem  localStorage.setItem
 import {ENTER_KEY, c, d, j, ls} from './helpers'
+import Task from './task'
+
 export default class ToDoList{
     constructor(key){ //Recibe la llave con la que guardara el localStorage
         this.key = key
 
         if( !ls.getItem(key) )
             ls.setItem( key, j.stringify([]) )
+
+            this.addTask = this.addTask.bind(this)
     }
 
     //Metodo para agregar tarea -> Manejador de eventos ya que lo va a manipular el input (desencadenara el metodo)
@@ -13,8 +17,15 @@ export default class ToDoList{
         if( !e.target.value) //Objeto que origina el evento target
             alert('No puedes agregar una tarea vacía')
 
-        if( e.keyCode === ENTER_KEY ) //Le da semantica 
-        let newTask =  new Task( e.target.value )
+        if( e.keyCode === ENTER_KEY ){ //Le da semantica 
+            let newTask =  new Task( e.target.value ),
+                tasks = j.parse( ls.getItem( this.key ) ) //Analizar en formato json lo que tenga ls
+
+                tasks.push( newTask )
+                ls.setItem( this.key, j.stringify( tasks ) )
+
+                c( this.key, newTask, ls)
+        }
     }
 
     render(){
