32b0c146d6988c32b16b2aa3fe2e14f2a4a4876a
kikiondo <kikisvv@ucol.mx>
2018-02-13 13:30:50 -0600

    Creado el condicional para saver si la tarea está vacía y un condicional para que solo se active con el ENTER_KEY

--- src/js/ToDoList.js
+++ src/js/ToDoList.js
@@ -8,7 +8,16 @@ export default class ToDoList{
             ls.setItem( key, j.stringify([]) )
     }
 
+    //Metodo para agregar tarea -> Manejador de eventos ya que lo va a manipular el input (desencadenara el metodo)
+    addTask(e){
+        if( !e.target.value) //Objeto que origina el evento target
+            alert('No puedes agregar una tarea vacía')
+
+        if( e.keyCode === ENTER_KEY ) //Le da semantica 
+        let newTask =  new Task( e.target.value )
+    }
+
     render(){
-        
+        task.addEventListener('keyup', this.addTask) //Le estoy diciendo que cuando el input haga algo desencadene un evento   
     }
 }
