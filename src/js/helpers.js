b830030229d2b9e5b0ab232b9f6f150c654a8c76
kikiondo <kikisvv@ucol.mx>
2018-02-12 18:36:20 -0600

    Se configuró el archivo helpes con los elementos de DOM y exportados por destructuración

---
+++ src/js/helpers.js
@@ -0,0 +1,17 @@
+//Filosofia de los modulos
+//Los elementos del dom los voy a guardar y exportar
+
+const ENTER_KEY = 13,
+    c = console.log,
+    d = document,
+    j = json,
+    ls = localStorage
+
+// Destructuración
+    export{
+        ENTER_KEY,
+        c,
+        d,
+        j,
+        ls
+    }
